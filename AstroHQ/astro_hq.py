9b2a2595219bf172941bc4903079c72819dff478
Caden Gobat <36030084+cgobat@users.noreply.github.com>
2023-10-10 23:06:15 -0600

    FITS header updates + more/better controls

--- AstroHQ/astro_hq.py
+++ AstroHQ/astro_hq.py
@@ -1,12 +1,16 @@
 import time
+import psutil
 import argparse
 import numpy as np
 from astropy.io import fits
 from picamera2 import Picamera2, libcamera
 
+BOOT_TIME = psutil.boot_time()
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--exposure", type=int, default=1, help="exposure time in seconds")
+parser.add_argument("-t", "--exposure", type=float, default=1., help="exposure time in seconds")
 parser.add_argument("-o", "--out-file", type=str, default="test.fits", help="file name/path to save output FITS image")
+parser.add_argument("-n", "--number", type=int, default=1, help="number of frames to capture in sequence")
 
 class PiHQCamera(Picamera2):
 
@@ -18,11 +22,16 @@ class PiHQCamera(Picamera2):
                                                                        "NoiseReductionMode": libcamera.controls.draft.NoiseReductionModeEnum.Off})
 
     @property
+    def format(self) -> str:
+        return self.sensor_format.split("_")[0]
+
+    @property
     def configuration(self) -> dict:
         return self.camera_config
     
     @configuration.setter
     def configuration(self, new_config: dict) -> None:
+        print("Configuring camera:", new_config)
         self.configure(new_config)
     
     @property
@@ -34,11 +43,11 @@ class PiHQCamera(Picamera2):
         
     @exposure.setter
     def exposure(self, set_val: float) -> None:
-        self.controls.ExposureTime = int(set_val*1e6)
+        self.controls.ExposureTime = int(set_val*1_000_000)
     
-    def capture_raw_array(self, metadata=False) -> np.ndarray:
+    def capture_raw_array(self, metadata=False) -> "np.ndarray|tuple[np.ndarray, dict]":
         request = self.capture_request()
-        array = request.make_array("raw")
+        array: np.ndarray = request.make_array("raw")
         if metadata:
             capture_meta = request.get_metadata()
         request.release()
@@ -53,21 +62,30 @@ class PiHQCamera(Picamera2):
     def capture_hdu(self) -> fits.PrimaryHDU:
         array, meta = self.capture_raw_array(metadata=True)
         hcrop, vcrop, width, height = meta["ScalerCrop"]
-        hdu = fits.PrimaryHDU(data=array[vcrop:vcrop+height, hcrop:hcrop+width])
-        hdu.header.set("DATE", time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime()), "[ISO cal] UTC time of HDU creation")
-        # hdu.header.set("DATE-OBS")
+        bpp = 12 # TODO
+        black_pt = np.unique(meta["SensorBlackLevels"]).item()//(2**(16-bpp)) # 2**16 / 2**12 = 16
+        array_cropped = array[vcrop:vcrop+height, hcrop:hcrop+width]
+        print(f"Orig. array min/max: {array_cropped.min(), array_cropped.max()}")
+        hdu = fits.PrimaryHDU(data=array_cropped.astype(np.uint16))
+        hdu.header.set("BUNIT", "DN", "units of array values")
+        hdu.header.set("FORMAT", self.format, "configured camera format")
+        hdu.header.set("INSTRUME", "Raspberry Pi HQ Camera Module", "camera name")
+        hdu.header.set("TELESCOP", None, "telescope model/name")
         hdu.header.set("DETECTOR", self.camera_properties["Model"].upper(), "camera sensor model")
         hdu.header.set("XPIXSIZE", self.camera_properties["UnitCellSize"][0]/1000, "[um] pixel width")
         hdu.header.set("YPIXSIZE", self.camera_properties["UnitCellSize"][1]/1000, "[um] pixel height")
         hdu.header.set("BAYER", ["RGGB", "GRBG", "GBRG", "BGGR", None][self.camera_properties["ColorFilterArrangement"]],
                        "Bayer matrix layout")
+        hdu.header.set("FRAMELUX", meta["Lux"], "[lx] estimated scene brightness/illuminance")
         hdu.header.set("UPTIME", meta["SensorTimestamp"]/1e9, "[s] system uptime since boot")
         hdu.header.set("CCD-TEMP", meta["SensorTemperature"], "[degC] SensorTemperature")
+        hdu.header.set("DATE-OBS", time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(BOOT_TIME+meta["SensorTimestamp"]/1e9)),
+                       "[ISO cal] observation timestamp, UTC")
         hdu.header.set("EXPTIME", meta["ExposureTime"]/1e6, "[s] ExposureTime")
-        hdu.header.set("DURATION", meta["FrameDuration"]/1e6, "[s] FrameDuration")
-        hdu.header.set("BLACKPT", meta["SensorBlackLevels"][0], "black point")
-        hdu.header["HISTORY"] = "Values scaled by factor of 16 to use full 16-bit range for original 12-bit pixels."
-        # hdu.scale(bzero=meta["SensorBlackLevels"][0])
+        hdu.header.set("DATAMIN", black_pt, "sensor black point")
+        hdu.header.set("DATAMAX", 2**bpp-1, f"maximum representable value with {bpp} bits")
+        hdu.header.set("DATE", time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime()), "[ISO cal] UTC time of HDU creation")
+ 
         hdu.add_checksum()
         return hdu
     
@@ -90,5 +108,12 @@ if __name__ == "__main__":
     print("Exposure time:", hqcam.exposure)
     print("Configuration:", hqcam.configuration)
 
-    hqcam.start_and_capture_fits(args.out_file)
+    if args.number==1:
+        hqcam.start_and_capture_fits(args.out_file)
+    else:
+        hqcam.start()
+        for i in range(args.number):
+            hqcam.capture_fits(args.out_file.format(i))
+            print(f"Captured {args.out_file.format(i)}")
     hqcam.stop()
+    hqcam.close()
