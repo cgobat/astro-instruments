e591c13343862850137a70c7768c2f4d1aa2e6c0
Caden Gobat <36030084+cgobat@users.noreply.github.com>
2023-10-12 17:01:50 -0600

    Add timestamp conversion utility; correct Bayer flip

--- AstroHQ/astro_hq.py
+++ AstroHQ/astro_hq.py
@@ -1,13 +1,14 @@
 import re
 import psutil
 import argparse
-import datetime as dt
+import time, datetime as dt
 import numpy as np
 from astropy.io import fits
 from picamera2 import Picamera2
 from libcamera import controls, Transform
 
 BOOT_TIME = dt.datetime.utcfromtimestamp(psutil.boot_time())
+sensortime_to_datetime = lambda sensortime_ns: BOOT_TIME+dt.timedelta(microseconds=sensortime_ns/1e3)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--exposure", type=float, default=1., help="exposure time in seconds")
@@ -21,9 +22,10 @@ class PiHQCamera(Picamera2):
         self.configuration = self.create_still_configuration(transform=Transform(), #hflip=False, vflip=True),
                                                              raw={"size": self.sensor_resolution,
                                                                   "format": self.sensor_format.replace("_CSI2P", "")},
-                                                             controls={"AnalogueGain": 1.0, "ColourGains": (1., 1.), "AwbEnable": False,
+                                                             controls={"AnalogueGain": 1.0, "ColourGains": (1., 1.),
+                                                                       "AwbEnable": False, "AeEnable": False, "Sharpness": 0.,
                                                                        "NoiseReductionMode": controls.draft.NoiseReductionModeEnum.Off})
-
+    
     @property
     def raw_format(self) -> str:
         return self.configuration["raw"]["format"]
@@ -43,62 +45,62 @@ class PiHQCamera(Picamera2):
             return self.controls.ExposureTime/1e6
         else:
             return np.nan
-        
+    
     @exposure.setter
     def exposure(self, set_val: float) -> None:
-        self.controls.ExposureTime = int(set_val*1_000_000)
+        self.controls.ExposureTime = round(set_val*1_000_000)
     
     def capture_raw_array(self, metadata=False) -> "np.ndarray|tuple[np.ndarray, dict]":
-        print(f"Capture start: {dt.datetime.utcnow().isoformat()}")
+        print(f"Capture starting at  {dt.datetime.utcnow().isoformat()}")
         request = self.capture_request()
         array: np.ndarray = request.make_array("raw")
         if metadata:
             capture_meta = request.get_metadata()
         print(f"Releasing request at {dt.datetime.utcnow().isoformat()}")
         request.release()
-        low_bytes = array[:, 0::2].astype(np.uint16) & 255
-        high_bytes = array[:, 1::2].astype(np.uint16) << 8
-        array_rebuilt = high_bytes + low_bytes
+        array_rebuilt = array.view("<u2")
         if metadata:
             return array_rebuilt, capture_meta
         else:
             return array_rebuilt
-            
+    
     def capture_hdu(self, crop=True) -> fits.PrimaryHDU:
         array, meta = self.capture_raw_array(metadata=True)
         print(f"\nCapture metadata:\n{meta}\n")
         raw_format = re.match(r"(S)(?P<bayer>[RGB]{4})(?P<bits>\d+)(_CSI2P)?", self.raw_format)
         bpp = int(raw_format.group("bits"))
         black_pt = np.unique(meta["SensorBlackLevels"]).item()//(2**(16-bpp)) # 2**16 / 2**12 = 16
+        bayer_order = raw_format.group("bayer")[2:] + raw_format.group("bayer")[:2] # flip vertically to match FITS
         if crop:
             hcrop, vcrop, width, height = meta["ScalerCrop"]
-            array_cropped = array[vcrop:vcrop+height, hcrop:hcrop+width]
-            print(f"Orig. array min/max: {array_cropped.min(), array_cropped.max()}")
-            hdu = fits.PrimaryHDU(data=array_cropped[::-1, :].astype(np.uint16))
-        else:
-            print(f"Orig. array min/max: {array.min(), array.max()}")
-            hdu = fits.PrimaryHDU(data=array[::-1, :].astype(np.uint16))
+            array = array[vcrop:vcrop+height, hcrop:hcrop+width]
+        print(f"Orig. array min/max: {array.min(), array.max()}")
+        hdu = fits.PrimaryHDU(data=array[::-1, :].astype(np.uint16))
         hdu.header.set("BUNIT", "DN", "units of array values")
         hdu.header.set("INST-SEP", "-"*19+" INSTRUMENT/CAMERA PROPERTIES "+"-"*19)
         # hdu.header.set("FORMAT", raw_format, "configured camera format")
         hdu.header.set("INSTRUME", "Raspberry Pi HQ Camera Module", "camera name")
         hdu.header.set("TELESCOP", None, "telescope model/name")
+        hdu.header.set("FOCALLEN", None, "[mm] telescope/lens focal length")
         hdu.header.set("DETECTOR", self.camera_properties["Model"].upper(), "camera sensor model")
         hdu.header.set("XPIXSIZE", self.camera_properties["UnitCellSize"][0]/1000, "[um] pixel width")
         hdu.header.set("YPIXSIZE", self.camera_properties["UnitCellSize"][1]/1000, "[um] pixel height")
-        hdu.header.set("BAYERPAT", raw_format.group("bayer"), "Bayer filter order/layout")
+        hdu.header.set("BAYERPAT", bayer_order, "Bayer filter order/layout")
         hdu.header.set("BITDEPTH", bpp, "number of bits per pixel value")
-        hdu.header.set("OBS-SEP", "-"*23+" OBSERVATION METADATA "+"-"*23)
+        hdu.header.set("META-SEP", "-"*23+" OBSERVATION METADATA "+"-"*23)
         hdu.header.set("FRAMELUX", meta["Lux"], "[lx] estimated scene brightness/illuminance")
-        hdu.header.set("UPTIME", meta["SensorTimestamp"]/1e9, "[s] system uptime since boot")
-        hdu.header.set("CCD-TEMP", meta["SensorTemperature"], "[degC] SensorTemperature")
-        hdu.header.set("DATE-OBS", (BOOT_TIME+dt.timedelta(seconds=meta["SensorTimestamp"]/1e9)).isoformat(),
-                       "[ISO cal] observation tismestamp, UTC")
-        hdu.header.set("EXPTIME", meta["ExposureTime"]/1e6, "[s] ExposureTime")
-        hdu.header.set("DATAMIN", black_pt, "sensor black point")
-        hdu.header.set("DATAMAX", 2**bpp-1, f"maximum representable value with {bpp} bits")
-        hdu.header.set("DATE", dt.datetime.utcnow().isoformat(), "[ISO cal] UTC time of HDU creation")
- 
+        hdu.header.set("COLORTMP", meta["ColourTemperature"], "[K] estimated average color temperature")
+        hdu.header.set("FOCUSFOM", meta["FocusFoM"], "image focus figure of merit")
+        # hdu.header.set("UPTIME", meta["SensorTimestamp"]/1e9, "[s] system uptime since boot")
+        hdu.header.set("CCD-TEMP", meta["SensorTemperature"], "[degC] sensor/detector temperature")
+        hdu.header.set("DATE-END", sensortime_to_datetime(meta["SensorTimestamp"]).isoformat(),
+                       "[ISO UTC] time of first pixel readout")
+        hdu.header.set("EXPTIME", meta["ExposureTime"]/1e6, "[s] image exposure time")
+        hdu.header.set("DATAMIN", black_pt, "[DN] sensor black point")
+        hdu.header.set("DATAMAX", 2**bpp-1, f"[DN] maximum representable value with {bpp} bits")
+        hdu.header.set("DATE", dt.datetime.utcnow().isoformat(), "[ISO UTC] time of HDU creation")
+        hdu.header.set("PROGRAM", "AstroHQ by cgobat", "software that generated this file")
+         
         hdu.add_checksum()
         return hdu
     
@@ -106,14 +108,19 @@ class PiHQCamera(Picamera2):
         hdu = self.capture_hdu()
         hdu.writeto(filename, overwrite=True)
         return
-
+    
     def start_and_capture_fits(self, filename: str) -> None:
         self.start()
+        throwaway = self.capture_metadata()
+        print("Initial metadata:", {kw: () if kw.endswith("CorrectionMatrix")
+                                        else sensortime_to_datetime(throwaway[kw]) if kw=="SensorTimestamp"
+                                        else throwaway[kw] for kw in throwaway})
         self.capture_fits(filename)
         return
 
 
 if __name__ == "__main__":
+    print(f"Execution start: {dt.datetime.utcnow()} UTC")
     args = parser.parse_args()
 
     hqcam = PiHQCamera()
@@ -124,6 +131,7 @@ if __name__ == "__main__":
         hqcam.start_and_capture_fits(args.out_file)
     else:
         hqcam.start()
+        time.sleep(1)
         for i in range(args.number):
             hqcam.capture_fits(args.out_file.format(i))
             print(f"Captured {args.out_file.format(i)}")
