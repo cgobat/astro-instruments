fb9e670d729110314e89139cfd784c7149303c52
Caden Gobat <36030084+cgobat@users.noreply.github.com>
2024-05-17 13:53:21 -0600

    Update NTP sync to set RTC instead of tracking a delta

--- peltier-tec/time_utils.py
+++ peltier-tec/time_utils.py
@@ -1,10 +1,11 @@
+import rtc
 import time
 import wifi
 import adafruit_ntp
 import socketpool
 
-EPOCH = time.mktime(time.struct_time((2020, 1, 1, 0, 0, 0, -1, -1, -1)))
-CLK_DELTA: int = None
+EPOCH = time.mktime(time.struct_time((2000, 1, 1, 0, 0, 0, -1, -1, -1)))
+RTC_VALID: bool = False
 
 def isoformat(time_struct = None, doy = False) -> str:
     if time_struct is None: # if no input, use current time
@@ -21,14 +22,13 @@ def isoformat(time_struct = None, doy = False) -> str:
 def ntp_sync():
     pool = socketpool.SocketPool(wifi.radio)
     ntp = adafruit_ntp.NTP(pool, tz_offset=0)
-    localtime = time.localtime()
-    ntp_datetime: time.struct_time = ntp.datetime
-    global CLK_DELTA
-    CLK_DELTA = time.mktime(ntp_datetime) - time.mktime(localtime)
+    RTC = rtc.RTC()
+    RTC.datetime = ntp.datetime
+    global RTC_VALID
+    RTC_VALID = True
 
 def now():
-    global CLK_DELTA
-    if CLK_DELTA is None:
+    global RTC_VALID
+    if not RTC_VALID:
         print("Warning: time has not been synchronized!")
-        return time.localtime()
-    return time.localtime(CLK_DELTA + time.time())
+    return time.localtime()
